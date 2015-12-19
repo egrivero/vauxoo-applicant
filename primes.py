51d121f8a8ceb796ca5912c87eee35de09254dc5
egrivero <edgarrivero@gmail.com>
2015-12-19 16:18:41 -0430

    Complete py1

--- primes.py
+++ primes.py
@@ -1,16 +1,22 @@
 """
-Your module documentation here
+Module created for the management of primes
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Class created for the management of primes
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Function to determine if a number is prime
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        prime = True
+	num_ord = 0
+	for i in range(num_int,0,-1):
+	    if num_int%i == 0:
+	        num_ord = num_ord+1
+	if num_ord > 2:
+	    prime = False
+        return prime
