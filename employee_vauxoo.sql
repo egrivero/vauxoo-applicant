a33c17ae801ee33c64893958a7d89b46f2e8a3be
egrivero <edgarrivero@gmail.com>
2015-12-19 12:49:58 -0430

    Se crean valores no nulos, se hace asocia departamento al empleado y se crean insert

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,14 +4,14 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
- 	id serial PRIMARY KEY NOT NULL,
+ 	id serial PRIMARY KEY NOT NULL UNIQUE,
 	first_name varchar(255) NOT NULL,
 	last_name varchar(255),
-	departament_id int,
+	departament_id int references employee_department(id),
 );
 
 CREATE TABLE employee_department (
-	id serial PRIMARY KEY NOT NULL,
+	id serial PRIMARY KEY NOT NULL UNIQUE,
 	name varchar(255) NOT NULL,
 	description varchar(255),
 );
@@ -19,4 +19,9 @@ CREATE TABLE employee_department (
 CREATE TABLE employee_hobby (
 );
 
+INSERT INTO employee_department(name, description)
+VALUES ('Tecnologia', 'Tecnologia de la informacion'),('Cuentas Por Cobrar','Cobranzas'),('Cuentas Por Pagar','Pagos'),('Ventas','Ventas'),('Auditoria','Auditoria'),('Recursos Humanos','RRHH');
+
+INSERT INTO employee(first_name, last_name, departament_id)
+VALUES ('Edgar','Rivero', 1),('Mauricio','Sanchez', 1),('Yennifer','Santiago', 6),('Hernan','Neuman', 4);
 -- ...
